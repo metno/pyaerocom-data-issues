04e326c9857d22aae15e3a9dbe458dad6a02a571
Jonas Gliss <jonasgliss@gmail.com>
2019-03-20 14:39:33 +0100

    Added new to investigate gaps in scattering Barrow when evaluating flag column

--- ebas/read_pm25_fails_workshop.py
+++ ebas/read_pm25_fails_workshop.py
@@ -7,16 +7,26 @@ Created on Fri Mar 15 11:37:11 2019
 """
 
 import pyaerocom as pya
+import matplotlib.pyplot as plt
+plt.close('all')
 
-r = pya.io.ReadEbas()
-#d = r.read('sconcso4', station_names=['B*'])
+r = pya.io.ReadGridded('EMEP_rv4_2599_Rep2013emistrend')
 
-emep_r = pya.io.ReadGridded('EMEP_rv4_2599_Rep2013emistrend')#.read_var('sconcso4')
 
-print(emep_r)
-# =============================================================================
-# coldata = pya.colocation.colocate_gridded_ungridded(emep_data,
-#                                                     d,
-#                                                     'sconcso4',
-#                                                     harmonise_units=False)                               
-# =============================================================================
+emep_data = r.read_var('od550aer', start=2010)
+
+
+print(emep_data)
+
+emep_data.quickplot_map(0)
+
+r = pya.io.ReadUngridded()
+obs = r.read('AeronetSunV3Lev2.daily', ['od550aer', 'ang4487aer'])
+print(obs)
+
+coldata = pya.colocation.colocate_gridded_ungridded(emep_data,
+                                                    obs,
+                                                    var_name='od550aer',
+                                                    harmonise_units=False)                               
+
+coldata.plot_scatter()
